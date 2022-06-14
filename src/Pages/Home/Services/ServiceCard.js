f9b16990cad4a7921ee0e5d2cbe458bb581ffd71
fahimahammed <fahimahammed.cse@gmail.com>
2022-06-14 18:41:49 +0600

    all warning removed

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -3,12 +3,12 @@ import React from 'react';
 const ServiceCard = ({ singleService }) => {
     const { title, price, description, img } = singleService;
     return (
-        <div class="card w-96 bg-base-100 hover:shadow-2xl">
-            <figure class="px-10 pt-10">
+        <div className="card w-96 bg-base-100 hover:shadow-2xl">
+            <figure className="px-10 pt-10">
                 <img src={img} alt="Photo" className="rounded-xl w-[72px]" />
             </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">{title}</h2>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{title}</h2>
                 <h4>${price}</h4>
                 <p>{description}</p>
             </div>
